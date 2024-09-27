5a6edfdbea148fd4b65d5e9064394c4ccbfd2382
kva16 <kalebv73@gmail.com>
2024-09-26 22:26:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('navigateButton').addEventListener('click', function() {
+    
+    window.location.href = 'visualization.html';
+  });
+
+  document.getElementById('backtoMain').addEventListener('click', function() {
+  
+    window.location.href = 'index.html';
+  });
---
+++ vis.js
@@ -0,0 +1,48 @@
+<><svg width="500" height="300">
+    < />!-- Title -->
+    <text x="100" y="30" class="title">How many times I've done these things in the past week</text>
+
+    < />!-- TV Bar -->
+    <rect x="100" y="180" width="50" height="100" class="bar"></rect>
+    <text x="125" y="295" class="label" text-anchor="middle">TV Episodes</text>
+    <text x="125" y="175" class="value" text-anchor="middle">10</text>
+
+    < />!-- Soccer Bar -->
+    <rect x="200" y="240" width="50" height="40" class="bar"></rect>
+    <text x="225" y="295" class="label" text-anchor="middle">Soccer</text>
+    <text x="225" y="235" class="value" text-anchor="middle">4</text>
+
+    < />!-- Music Bar -->
+    <rect x="300" y="80" width="50" height="200" class="bar"></rect>
+    <text x="325" y="295" class="label" text-anchor="middle">Music</text>
+    <text x="325" y="75" class="value" text-anchor="middle">25</text>
+
+    < />!-- Gym Bar -->
+    <rect x="400" y="230" width="50" height="50" class="bar"></rect>
+    <text x="425" y="295" class="label" text-anchor="middle">Gym</text>
+    <text x="425" y="215" class="value" text-anchor="middle">5</text>
+</svg><svg width="500" height="300" style="margin-top: 100px;">
+        < />!-- Large Red Circle -->
+        <circle cx="100" cy="150" r="70" fill="red" />
+
+        < />!-- Blue Square Inside Red Circle -->
+        <rect x="65" y="115" width="150" height="70" fill="blue" />
+
+        < />!-- Green Triangle Overlapping Red Circle -->
+        <polygon points="100,50 150,130 50,130" fill="green" />
+
+        < />!-- Yellow Circle Overlapping Blue Square -->
+        <circle cx="210" cy="250" r="30" fill="yellow" />
+
+        < />!-- Light Blue Oval -->
+        <ellipse cx="300" cy="150" rx="80" ry="50" fill="lightblue" />
+
+        <rect x="300" y="150" width="70" height="125" fill="lightgreen" />
+        < />!-- Orange triangle -->
+        <polygon points="300,30 450,30 400,100" fill="orange" />
+
+        < />!-- Pink circles -->
+        <circle cx="200" cy="50" r="60" fill="pink" />
+        <circle cx="400" cy="250" r="15" fill="pink" />
+        <circle cx="50" cy="250" r="40" fill="pink" />
+    </svg></>
